32b52ec58665bc9e31044c68452ed60025e44d1c
HoangKhiem3k <ledohoangkhiem3k@gmail.com>
2023-05-06 10:38:14 +0700

    khiem-done-request-component

--- src/components/Common/Request/index.jsx
+++ src/components/Common/Request/index.jsx
@@ -1,17 +1,30 @@
 import React from 'react';
 import './Request.scss';
 export default function Request() {
-  let requestStatus = 'new';
+  // let requestStatus = 'new';
+  // let requestStatus = 'offered';
+  // let requestStatus = 'appointed';
+  // let requestStatus = 'done';
+  let requestStatus = 'canceled';
+
   return (
     <div className="request-container">
       <div className="request-service">
         <div className="request-service-left">
           <h3>Service name</h3>
           <p>Package name</p>
-          <p>Đã nhận 3 giờ trước</p>
+          <p>
+            {requestStatus === 'appointed'
+              ? 'Đã chấp nhận báo giá'
+              : requestStatus === 'canceled'
+              ? 'Đã bị hủy bởi khách'
+              : 'Đã nhận 3 giờ trước'}
+          </p>
         </div>
         <div className="request-service-right">
-          <button>Từ chối lịch hẹn</button>
+          <button className={requestStatus === 'done' || requestStatus === 'canceled' ? 'disable' : ''}>
+            Từ chối lịch hẹn
+          </button>
           <button>Chat</button>
         </div>
       </div>
@@ -42,13 +55,32 @@ export default function Request() {
           <div className="request-price-top-left">Price Icon</div>
           <div className="request-price-top-right">
             <h4>Báo giá</h4>
-            <p>Chưa báo giá</p>
+            <p>{requestStatus === 'new' ? 'Chua bao gia' : '70000d'}</p>
           </div>
         </div>
-        <div className="request-price-bottom">
+        <div className={requestStatus === 'new' ? 'request-price-bottom' : 'request-price-bottom disable'}>
           <p>Nhập báo giá tại đây</p>
           <input placeholder="Nhập giá"></input>
           <button>Gửi báo giá</button>
+        </div>
+        <div className={requestStatus === 'appointed' ? 'request-appointed' : 'request-appointed disable'}>
+          <button>Hoàn thành công việc</button>
+        </div>
+        <div className={requestStatus === 'done' ? 'request-done' : 'request-done disable'}>
+          <div className="provider-evaluate">
+            <div className="provider-evaluate-left">Like Icon</div>
+            <div className="provider-evaluate-right">
+              <p>Đánh giá của bạn</p>
+              <h4>Có tiền tip</h4>
+            </div>
+          </div>
+          <div className="customer-evaluate">
+            <div className="customer-evaluate-left">Star Icon</div>
+            <div className="customer-evaluate-right">
+              <p>Đánh giá của khách</p>
+              <h4>5/5</h4>
+            </div>
+          </div>
         </div>
       </div>
     </div>
