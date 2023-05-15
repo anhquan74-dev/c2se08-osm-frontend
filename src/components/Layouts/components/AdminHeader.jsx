c44de4f0d2aa5a5f6ffc1249a2af07ec00e0878c
ThuanHuynh14 <huynhvanthuan.140921@gmail.com>
2023-05-15 16:00:53 +0700

    Handle logout

--- src/components/Layouts/components/AdminHeader.jsx
+++ src/components/Layouts/components/AdminHeader.jsx
@@ -1,15 +1,28 @@
 import {
   AppBar,
-  Box,
   Button,
-  IconButton,
   Toolbar,
-  Typography,
+  Typography
 } from "@mui/material";
-import React from "react";
-import MenuIcon from "@mui/icons-material/Menu";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { logOut } from "../../../modules/Auth/authSlice";
 
 const AdminHeader = () => {
+  const { currentUser } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!currentUser) {
+      navigate('/login');
+    }
+  }, [currentUser]);
+
+  const handleLogout = (e) => {
+    e.preventDefault();
+    dispatch(logOut());
+  };
   return (
     // <Box sx={{ flexGrow: 1 }}>
     <AppBar position="static" sx={{ height: "100%" }}>
@@ -17,7 +30,7 @@ const AdminHeader = () => {
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
           OSM Management
         </Typography>
-        <Button color="inherit">Log out</Button>
+        <Button color="inherit" onClick={handleLogout}>Log out</Button>
       </Toolbar>
     </AppBar>
     // </Box>
