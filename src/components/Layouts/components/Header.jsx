8733b860825b92eb143524acd3574992f4789f79
anhquan74-dev <anhquan74.dev@gmail.com>
2023-05-23 18:28:00 +0700

    fix header footer

--- src/components/Layouts/components/Header.jsx
+++ src/components/Layouts/components/Header.jsx
@@ -106,13 +106,10 @@ const Header = () => {
         <li>
           <div onClick={handleClick}>
             <div>
-              <img
-                src="https://icdn.dantri.com.vn/thumb_w/680/2023/04/01/afp-messi-1-167911043942020387261-75-0-625-881-crop-1679110702236160038944-1679118122610-1680330659064.jpeg"
-                alt="avatar"
-              />
+              <img src={currentUser?.avatar?.url} alt="avatar" />
             </div>
             <div>
-              TRAN ANH QUAN
+              {currentUser?.full_name}
               <ArrowDropDownIcon />
             </div>
           </div>
